58ed6bc78347bae04cad8f4cd137057efeeb7b8b
Stephanie Simms <stephanie.simms@gmail.com>
2019-09-01 14:47:42 -0700

    refactored card component to calculate transform and store as props

--- src/Card.js
+++ src/Card.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import './Card.css';
+import { directive } from '@babel/types';
 
-export default class Card extends Component {
+class Card extends Component {
   constructor(props) {
     super(props);
-  }
 
-  render() {
-    const { imgUrl, id } = this.props;
+    //Calculate randomly rotated card position and store as instance variable
     let angle = Math.random() * 90 - 45;
     let randomX = Math.random() * 40 - 20;
     let randomY = Math.random() * 40 - 20;
-    
+    this._transform = `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`;
+  }
+
+  render() {
+    const { imgUrl, name } = this.props;
+
     return (
-      <div className="Card-area">
-        <img src={`${imgUrl}`} style={{ transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)` }}></img>
-      </div>
-    )
+      <img className="Card"
+        alt={name}
+        src={`${imgUrl}`}
+        style={{ transform: this._transform }} />
+    );
   }
 }
 
-// style={{transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`}}
+export default Card;
